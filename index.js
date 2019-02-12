52143313b8e18f23ac303e184f24fba37f1878b0
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-12 01:45:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 
-function boardMember(name, homeState, training) {
-  this.name = name;
-  this.homeState = homeState;
-  this.training = training;
-  
-  this.veto = function() {
-    return "No, I must disagree"
-  }
+function BoardMember (name, homeState, training){
+	this.name = name;
+	this.homeState = homeState;
+	this.training = training;
+
+	this.veto = function(){
+		return "No, I must disagree"
+	}
 
 
 
